3faf34357e86106111f6abc5263538143f04a4b5
kimtaehyun84 <kimtaehyun84@gmail.com>
2021-02-24 17:28:15 +0900

    add new files

---
+++ src/main/java/com/kimtaehyun84/springboot_webservice/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kimtaehyun84.springboot_webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/com/kimtaehyun84/springboot_webservice/web/IndexController.java
+++ src/main/java/com/kimtaehyun84/springboot_webservice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kimtaehyun84.springboot_webservice.web;
 
+import com.kimtaehyun84.springboot_webservice.config.auth.LoginUser;
+import com.kimtaehyun84.springboot_webservice.config.auth.dto.SessionUser;
 import com.kimtaehyun84.springboot_webservice.service.posts.PostsService;
 import com.kimtaehyun84.springboot_webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
